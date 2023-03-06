0c5d0355ed29e0633dc6a7f05729496d57f029d1
Lukas Lerche <lukas.lerche@tu-dortmund.de>
2023-03-06 17:28:00 +0100

    Added basic list of pkmn

---
+++ src/routes/list/+page.server.ts
@@ -0,0 +1,19 @@
+import PokeAPI from 'pokedex-promise-v2';
+import type { PageServerLoad } from './$types';
+
+export const load = (async () => {
+	const P = new PokeAPI();
+
+	const interval = {
+		limit: 151,
+		offset: 0
+	};
+	const list = await P.getPokemonsList(interval);
+	console.log(
+		`The result can serve ${list.count} pkmn, but i only requested an interval of ${list.results.length} pkmn`
+	);
+
+	return {
+		list: list.results
+	};
+}) satisfies PageServerLoad;
---
+++ src/routes/list/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	const list = data.list;
+</script>
+
+<h1>List of Pokémon</h1>
+<ul>
+	{#each list as p}
+		<li>{p.name} : {p.url}</li>
+	{/each}
+</ul>
