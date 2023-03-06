fcaf3b10b5927e9466c56888f36dabbb2486da1a
Lukas Lerche <lukas.lerche@tu-dortmund.de>
2023-03-06 18:23:04 +0100

    Add error for wrong pkmn id

--- src/routes/pkmn/[id]/+page.server.ts
+++ src/routes/pkmn/[id]/+page.server.ts
@@ -1,11 +1,18 @@
 import PokeAPI from 'pokedex-promise-v2';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ params }) => {
 	const P = new PokeAPI(); // TODO does this cache if we create a new one every request?
 
 	console.log(`Requesting pkmn no. ${params.id}`);
-	const pokemon = await P.getPokemonByName(params.id);
+    let pokemon
+    try {
+        pokemon = await P.getPokemonByName(params.id);
+    } catch (err) {
+        throw error(404, "Your Pokémon is in another castle!");
+    }
+    
 	console.log(pokemon);
 
 	return { pokemon };
