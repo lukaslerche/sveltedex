f99903ba5a7ecf8d78f6bf6f5165137bc5e4fb28
Lukas Lerche <lukas.lerche@tu-dortmund.de>
2023-03-07 11:31:01 +0100

    Added nes.css for inner content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Pokédex</h1>
-<a href="/list">List of Pokémon</a>
+<a href="/pkmn">List of Pokémon</a>
 <a href="/pkmn/6">Get Charizard</a>
---
+++ src/routes/pkmn/+layout.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet" />
+	<link href="https://unpkg.com/nes.css/css/nes.min.css" rel="stylesheet" />
+</svelte:head>
+<slot />
