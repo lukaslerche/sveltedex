47de710678af8288fa23c3f7735fb59e17a5e211
Lukas Lerche <lukas.lerche@tu-dortmund.de>
2023-03-07 12:01:22 +0100

    Made the links in the list work

--- src/routes/pkmn/+page.server.ts
+++ src/routes/pkmn/+page.server.ts
@@ -1,4 +1,5 @@
 import P from '$lib/server/PokeAPI';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
@@ -6,10 +7,22 @@ export const load = (async () => {
 		limit: 151,
 		offset: 0
 	};
-	const list = await P.getPokemonsList(interval);
-	console.log(
-		`The result can serve ${list.count} pkmn, but i only requested an interval of ${list.results.length} pkmn`
-	);
+
+	let list;
+	try {
+		list = await P.getPokemonsList(interval);
+		console.log(
+			`The result can serve ${list.count} pkmn, but i only requested an interval of ${list.results.length} pkmn`
+		);
+
+		/*list.results.forEach(async (pokemon) => {
+			const pokemonDetail = await P.getPokemonByName(pokemon.name);
+			console.log(pokemonDetail.id);
+			
+		});*/
+	} catch (err) {
+		throw error(404, 'Could not retrieve pkmn.');
+	}
 
 	return {
 		list: list.results
--- src/routes/pkmn/+page.svelte
+++ src/routes/pkmn/+page.svelte
@@ -4,11 +4,21 @@
 	export let data: PageData;
 
 	$: list = data.list;
+
+	function cap(str: string): string {
+		return str.charAt(0).toUpperCase() + str.slice(1);
+	}
+
+	function idFromUrl(url: string) {
+		const regex = /\/(\d+)\/$/;
+		const result = url.match(regex);
+		return result ? result[1] : '';
+	}
 </script>
 
 <h1>List of Pokémon</h1>
 <ul>
 	{#each list as p}
-		<li>{p.name} : {p.url}</li>
+		<li><a href="/pkmn/{idFromUrl(p.url)}">{idFromUrl(p.url)}: {cap(p.name)}</a></li>
 	{/each}
 </ul>
