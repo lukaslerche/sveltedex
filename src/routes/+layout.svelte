abb5cc9bae72b79c613f5c5c90608edd5b0c87a3
Laconfire <11411471+Laconfire@users.noreply.github.com>
2023-03-07 14:49:29 +0100

    Pokedex Header Style

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import 'modern-normalize';
+    import '$lib/pokedex.css';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+</script>
+
+<div class="pokedexWrapper">
+	<div class="pokedexTop left">
+		<div id="top_left">
+			<div id="buttonGlass">
+				<div id="reflect" />
+			</div>
+			<div id="miniButtonGlass1" />
+			<div id="miniButtonGlass2" />
+			<div id="miniButtonGlass3" />
+		</div>
+		<div class="block1 left" />
+		<div class="block2 left" />
+		<div class="block3 left" />
+	</div>
+	<div class="pokedexTop right">
+		<div class="block1 right" />
+		<div class="block2 right" />
+		<div class="block3 right" />
+	</div>
+	<div class="content"><slot /></div>
+	<div class="pokedexBot left">Four</div>
+	<div class="pokedexBot right">Five</div>
+</div>
+
+<style>
+	* {
+		padding: 0;
+		margin: 0;
+	}
+	
+</style>
