73fd9196707188a222be4a8d95f04c348c235496
Laconfire <11411471+Laconfire@users.noreply.github.com>
2023-03-08 10:33:40 +0100

    WIP: Icon Animaion

--- src/routes/pkmn/[id]/+page.svelte
+++ src/routes/pkmn/[id]/+page.svelte
@@ -6,22 +6,28 @@
 
 	$: p = data.pokemon;
 	$: e = data.nextEvolutions;
+	console.log(data.pokemon);
+	let imgUrl = p.sprites.other['official-artwork'].front_default;
 </script>
+
 
 <h1>No. {p.id} - {cap(p.name)}</h1>
 
-<div><img src={p.sprites.front_default} alt="sprite of {p.name}" /></div>
+<div>
+	<!-- <div --background-image="url({imgUrl}})" /> -->
+	<img class='bw pokeImage' src={p.sprites.other['official-artwork'].front_default} alt="sprite of {p.name}" />
+</div>
 
 <div class="nes-container with-title">
 	<p class="title">Info</p>
 	<p>
 		Types:
-		{#each p.types as t}
-			{t.type.name}&nbsp;
+		{#each p.types as t, i}
+			{cap(t.type.name)}{#if i < p.types.length - 1},&nbsp;{/if}
 		{/each}
 	</p>
-	<p>Height: {p.height} inch</p>
-	<p>Weight: {p.weight} lb</p>
+	<p>Height: {p.height / 10} m</p>
+	<p>Weight: {p.weight / 10} kg</p>
 </div>
 
 <div class="nes-container with-title">
@@ -39,12 +45,12 @@
 </div>
 
 {#if e.length > 0}
-<div class="nes-container with-title">
-	<p class="title">Evolution</p>
-	{#each e as evo}
-		<p>{cap(evo.species.name)}</p>
-	{/each}
-</div>
+	<div class="nes-container with-title">
+		<p class="title">Evolution</p>
+		{#each e as evo}
+			<p>{cap(evo.species.name)}</p>
+		{/each}
+	</div>
 {/if}
 
 <div>
