83247713a912f769d7c54f5df63d11b51bfd63cf
Laconfire <11411471+Laconfire@users.noreply.github.com>
2023-03-08 09:19:07 +0100

    TS Types in dexUtils importiert

--- src/lib/dexUtils.ts
+++ src/lib/dexUtils.ts
@@ -1,16 +1,17 @@
-function isEeveelution (evoChain: PokeAPI.Chain, name: string): Boolean {
-    return (evoChain.species.name == 'eevee' && name != 'eevee');
+import type { Chain, ChainEvolvesTo } from 'pokedex-promise-v2';
+
+function isEeveelution(evoChain: Chain, name: string): boolean {
+	return evoChain.species.name == 'eevee' && name != 'eevee';
 }
 
-function getNextEvolutions (evoChain: PokeAPI.Chain, ownName: string): PokeAPI.ChainEvolvesTo[] {
-    if (isEeveelution(evoChain, ownName)) {
-        return evoChain.evolves_to[0].evolves_to;
-    }
-    while (ownName != evoChain.species.name) {
-        evoChain = evoChain.evolves_to[0];
-        console.log(evoChain);
-    }
-    return evoChain.evolves_to;
+function getNextEvolutions(evoChain: Chain, ownName: string): ChainEvolvesTo[] {
+	if (isEeveelution(evoChain, ownName)) {
+		return evoChain.evolves_to[0].evolves_to;
+	}
+	while (ownName != evoChain.species.name) {
+		evoChain = evoChain.evolves_to[0];
+	}
+	return evoChain.evolves_to;
 }
 
 export { getNextEvolutions };
