c61a03c1dbf540df55ec4f6637f1d5ba8a092f92
Lukas Lerche <lukas.lerche@tu-dortmund.de>
2023-03-09 10:18:04 +0100

    Added diamond stats

---
+++ src/routes/pkmn/[id]/Stats.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import { Radar } from 'svelte-chartjs';
+	import {
+		Chart as ChartJS,
+		Title,
+		Tooltip,
+		Legend,
+		PointElement,
+		RadialLinearScale,
+		LineElement
+	} from 'chart.js';
+	ChartJS.register(Title, Tooltip, Legend, PointElement, RadialLinearScale, LineElement);
+
+
+    export let pkmn;
+
+	$: statdata = {
+		labels: [ 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+		datasets: [
+			{
+				label: 'Stats',
+				data: [65, 59, 90, 81, 56, 55],
+				fill: true,
+				backgroundColor: 'rgba(255, 99, 132, 1)',
+				borderColor: 'rgb(255, 99, 132)',
+				pointBackgroundColor: 'rgb(255, 99, 132)',
+				pointBorderColor: '#fff',
+				pointHoverBackgroundColor: '#fff',
+				pointHoverBorderColor: 'rgb(255, 99, 132)'
+			}
+		]
+	};
+</script>
+<Radar data={statdata} />
